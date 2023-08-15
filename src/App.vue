fa9604be8ad400e77464f8dcd6421fe797730977
Parth Dedaniya <parthdedaniya45@gmail.com>
2023-08-15 18:14:54 +0530

    Add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
+import NavBar from './components/NavBar.vue';
+import Home from './views/Home.vue';
+import Footer from './components/Footer.vue';
 </script>
 
-<template></template>
+<template>
+  <NavBar />
+  <RouterView />
+  <Footer />
+</template>
 
-<style scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
